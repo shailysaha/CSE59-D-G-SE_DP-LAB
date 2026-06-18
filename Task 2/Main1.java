386b1b90f06c0b910eb8dbc290edc835861d205d
shailysaha <shailysaha46@gmail.com>
2026-06-18 15:55:36 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,14 @@
 
+ class ContestStats {
+    int[] scores = {85,90,95};
+    void printfirstScore(){
+        System.out.println("First Score : " +scores[0]);
+    }
+}
+public class Main1{
+    public static void main(String[] args) {
+        ContestStats contest = new ContestStats();
+        contest.printfirstScore();
+    }
+    
+}
