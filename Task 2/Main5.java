caf840e0460cc495e07bed30e27f275735ffda8e
shailysaha <shailysaha46@gmail.com>
2026-06-18 16:38:16 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,18 @@
 
+class Printer{
+    void printData(String text){
+        System.out.println("Print text: "+text);
+    }
+    void printData(int num){
+        System.out.println("Print number: "+num);
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        Printer print = new Printer();
+        print.printData("hello Java");
+        print.printData(100);
+    }
+    
+}
