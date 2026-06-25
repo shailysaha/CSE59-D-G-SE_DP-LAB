5424221592861e8afef96fc4bf32b96df3e5deed
shailysaha <shailysaha46@gmail.com>
2026-06-25 16:55:31 +0600

    Update Main1.java

--- Task 3/Main1.java
+++ Task 3/Main1.java
@@ -1,1 +1,36 @@
-
+public class Main1 {
+    public static void main(String[] args) {
+        User user = new User ("Dora", "Dora@gmail.com");
+        
+        UserRepository repository = new  UserRepository();
+        repository.save(user);
+    }
+}
+class User {
+    private String username;
+    private String email;
+   public User(String username, String email){
+        this.username = username;
+        this.email = email;
+    } public String getUsername(){
+        return username;
+    } public String getEmail(){
+        return email;
+    }
+} class EmailValidator {
+    public static boolean isValid(String email){
+        if(email != null && email.contains("@")){
+            return true;
+        }
+        System.out.println("Invalid email format");
+        return false;
+    }
+}
+class UserRepository {
+    public void save (User user){
+        if (EmailValidator.isValid(user.getEmail())){
+            System.out.println("Connecting to database...");
+            System.out.println("Saving user "+ user.getUsername() + "to the users table");
+        }
+    }
+}
